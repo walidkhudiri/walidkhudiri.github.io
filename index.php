bed8c96f157f624a8e67f60f6ef4582bce596dd8
walidkhudiri <59583948+walidkhudiri@users.noreply.github.com>
2021-03-07 03:16:18 +0200

    Update index.php

--- index.php
+++ index.php
@@ -389,16 +389,7 @@
 									</ul>
 								</div>
 							</section><!--//language-section-->
-							<section class="resume-section interests-section mb-5">
-								<h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Interests</h2>
-								<div class="resume-section-content">
-									<ul class="list-unstyled">
-										<li class="mb-1">Climbing</li>
-										<li class="mb-1">Snowboarding</li>
-										<li class="mb-1">Cooking</li>
-									</ul>
-								</div>
-							</section><!--//interests-section-->
+							
 							
 						</div>
 					</div><!--//row-->
